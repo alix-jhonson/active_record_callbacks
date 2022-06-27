0c89dd1c1a9e79219e8d82d39e89c5e481fc250c
rana haseeb <alix@yahoo.com>
2022-06-27 10:56:13 +0500

    callbacks touch_method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ActiveRecord::Base
-after_initialize do |user|
-    puts "You have initialized an object!"
-  end
-
-  after_find do |user|
-    puts "You have found an object!"
+after_touch do |user|
+    puts "You have touched an object"
   end
 end
-User.new
-User.first
+u = User.create(name: 'Kuldeep')
+u.touch
