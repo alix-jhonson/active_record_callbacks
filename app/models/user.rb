4c409c1f4a89f4aa0cdeb3c561aa52a61e801bd7
rana haseeb <alix@yahoo.com>
2022-06-27 10:45:02 +0500

    callbacks registeration through macro-style class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
   validates :login, :email, presence: true
 
-  before_validation :ensure_login_has_a_value
-
-  protected
-    def ensure_login_has_a_value
-      if login.nil?
-        self.login = email unless email.blank?
-      end
-    end
+  before_create do
+    self.name = login.capitalize if name.blank?
+  end
 end
