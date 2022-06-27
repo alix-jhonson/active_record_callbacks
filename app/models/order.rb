c2ecd3daecc735a92de4ce9d4dd3dc67eccc496b
rana haseeb <alix@yahoo.com>
2022-06-27 11:21:44 +0500

    unless with a Proc_callbacks

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
-  before_save :normalize_card_number, if: :paid_with_card?
+  before_save :normalize_card_number,
+    if: Proc.new { |order| order.paid_with_card? }
 end
